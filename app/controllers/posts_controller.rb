5e147ac30ea713fdc2d0af9a1b468d9c5d283ba2
Sharon Watkins <swatkins876@gmail.com>
2020-01-22 18:15:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,12 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
 end
