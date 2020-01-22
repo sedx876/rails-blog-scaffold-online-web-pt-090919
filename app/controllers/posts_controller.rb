06d36299b8643ad22e687697603b82cb39085ef7
Sharon Watkins <swatkins876@gmail.com>
2020-01-22 18:18:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,8 +23,18 @@ class PostsController < ApplicationController
   end
 
   def update
-  @post = Post.find(params[:id])
-  @post.update(post_params)
-  redirect_to post_path(@post)
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+ end
+
+ def destroy
+   @post = Post.destroy_all
+ end
+
+ private
+
+def post_params
+  params.require(:post).permit(:title, :description)
 end
 end
